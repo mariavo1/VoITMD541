86a4562c50467785b36894f733b7c111a0436636
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2022-10-21 15:06:14 -0500

    add onchange onto billtotal and tip

---
+++ Lab3/tipcalc.js
@@ -0,0 +1,4 @@
+document.getElementById('#form').onchange = function() {
+    let billTot = Number(document.getElementById('billTotal').value);
+    let tip = document.getElementById('tips').value;
+}
