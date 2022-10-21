238673136f3ed8ccd17847b0220322205a716738
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2022-10-21 16:36:16 -0500

    Add javascript

--- Lab3/tipcalc.js
+++ Lab3/tipcalc.js
@@ -1,4 +1,13 @@
-document.getElementById('#form').onchange = function() {
-    let billTot = Number(document.getElementById('billTotal').value);
-    let tip = document.getElementById('tips').value;
-}
+document.querySelector('#form').onchange = function() {
+    
+    let bill = Number(document.getElementById('billTotal').value);
+    let tip = document.getElementById('tips').value;   
+
+    let tipTotal = bill * (tip/100);  
+    let allTotal = bill + (bill * (tip/100));
+
+    document.getElementById('tipPercentage').value = tip;
+    document.getElementById('tipAmount').value = tipTotal.toFixed(2);
+    document.getElementById('billAndTip').value = allTotal.toFixed(2);
+
+};
