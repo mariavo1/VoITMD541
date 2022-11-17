6e88af8586b395a91e4af7e1b73ff8e3c4405448
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2022-11-17 17:16:53 -0600

    fetch weather api

--- Lab4/weather.js
+++ Lab4/weather.js
@@ -2,6 +2,15 @@ window.addEventListener('DOMContentLoaded', function(){
     let bar = document.getElementById("searchBar");
     let searchBtn = document.getElementById("search-btn");
     let geo = document.getElementById("geo-btn");
+    let lat = 0;
+    let long = 0;
+
+    function success(p) {
+        lat = p.coords.latitude;
+        long = p.coords.longitude;
+        let api = "https://weatherdbi.herokuapp.com/data/weather/" + lat + "," + long;
+        fetchApi(api);
+    }
 
     function currentLocation() {
         if (navigator.geolocation) {
