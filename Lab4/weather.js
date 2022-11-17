94c65257c64d72bc769f59a76fb34ee10477fc31
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2022-11-17 16:55:53 -0600

    get element by id

---
+++ Lab4/weather.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function(){
+    let bar = document.getElementById("searchBar");
+    let searchBtn = document.getElementById("search-btn");
+    let geo = document.getElementById("geo-btn");
+
+    function currentLocation() {
+        if
+    }
+}); //end
