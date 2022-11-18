e62616d917d353bf28eb8c8cb2c607b6eaa854ae
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2022-11-18 16:44:29 -0600

    add function to grab weather data

--- Lab4/weather.js
+++ Lab4/weather.js
@@ -9,4 +9,62 @@ window.addEventListener('DOMContentLoaded', function(){
     } else {
         errMsg.innerHTML = '<button id="searchbtn">Search</button>'
     }
+
+    function WeatherCast(){
+        let api = 'https://weatherdbi.herokuapp.com/data/weather/'
+
+        fetch(api+loc.value.replace(' ',''))
+        .then(function(res){
+        return res.json();      
+        })
+        .then(function(jsonData){
+            if (!jsonData.status) {
+                let tForecast = ""
+                tForecast+= "<div class='wForecast'>"
+                tForecast+= "<h3 id='day'>" + jsonData.currentConditions.dayhour + "</h3>"
+                tForecast+= "<p id='icon'>"+ jsonData.currentConditions.comment +"<img src=" + jsonData.currentConditions.iconURL   + "></p>"
+                tForecast+= "<p id='temp'>Temperature: " + jsonData.currentConditions.temp.f + "°F, Humidity: " +    jsonData.currentConditions.humidity + ", Wind: " + jsonData.currentConditions.wind.mile +  " mph</p>"
+                tForecast+= "</div>"  
+
+                document.getElementById("currentTemp").innerHTML = tForecast;
+
+                let r = document.getElementById("region");
+    
+                r.innerHTML = jsonData.region;
+    
+                let nextForecast = ""
+
+                for (var seven of jsonData.next_days.slice(1)) {
+
+                    nextForecast+= "<div class='wForecast'>"
+                    nextForecast+= "<h3 id='day'>" + seven.day + "</h3>"
+                    nextForecast+= "<p id='icon'>"+ seven.comment +"<img src=" + seven.iconURL   + "></p>"
+                    nextForecast+= "<p id='maxmin'> Max Temp: " + seven.max_temp.f + "°F , Min Temp: " +  seven.min_temp.f + "°F</p>"
+                    nextForecast+= "</div>"                    
+                }
+                    document.getElementById("nextTemp").innerHTML = nextForecast;
+            } else {
+                let r = document.getElementById("region");
+                r.innerHTML = '<h3> Error  : ' + jsonData.message + ' </h3>';
+                document.getElementById("currentTemp").innerHTML = "";
+                document.getElementById("nextTemp").innerHTML = "";
+            }
+            
+        })
+        .catch(error => console.log(error));
+    }
+/*
+    function loadingGif() {
+        let gifImg = document.getElementById("gifImg");
+        gifImg.show();
+        setTimeout(() => {
+            gifImg.hide();
+        }, 4000);
+    }
+
+    function hideLoading() {
+        document.getElementById("gifImg").style.display = 'none';
+    }
+*/
+
 }); //end
