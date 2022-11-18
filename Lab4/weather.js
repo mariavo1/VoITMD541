de394fc878b5bc8d9af516a9fb651704c91a7320
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2022-11-18 16:29:46 -0600

    Error message shown to user if nothing is typed in

--- Lab4/weather.js
+++ Lab4/weather.js
@@ -3,5 +3,10 @@ window.addEventListener('DOMContentLoaded', function(){
     let locbtn = this.document.getElementById("loc-btn")
     let loc = document.getElementById("loc");
     let errMsg = document.getElementById("errMsg");
-    
+
+    if (loc.value == '') {
+        errMsg.innerHTML = '<p>Please enter into the search bar</p><button id="searchbtn" disabled>Disabled</button>'
+    } else {
+        errMsg.innerHTML = '<button id="searchbtn">Search</button>'
+    }
 }); //end
