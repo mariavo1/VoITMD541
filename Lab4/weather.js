84e1f427e1b6d7b41553d4f2111089c92d569bf3
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2022-11-18 16:28:16 -0600

    grab elements

---
+++ Lab4/weather.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', function(){ 
+
+    let locbtn = this.document.getElementById("loc-btn")
+    let loc = document.getElementById("loc");
+    let errMsg = document.getElementById("errMsg");
+    
+}); //end
